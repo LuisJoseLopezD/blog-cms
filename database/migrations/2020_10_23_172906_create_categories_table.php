9be5868040611e21bb22e5613f7121c43e259f41
luisjoselopezd <luisjoselopezd@gmail.com>
2020-10-23 14:07:27 -0400

    second commit

--- database/migrations/2020_10_23_172906_create_categories_table.php
+++ database/migrations/2020_10_23_172906_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name',64);
+            $table->string('slug',128)->unique(); //url amigable. laravel new version don't allows "slug"
+            $table->mediumText('body',128)->nullable();
             $table->timestamps();
         });
     }
