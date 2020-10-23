be57fdf7a1a0a2c3d180dd41759ce7f255651128
luisjoselopezd <luisjoselopezd@gmail.com>
2020-10-23 05:44:37 -0400

    third commit

--- database/migrations/2020_10_23_173424_crate_post_tag_table.php
+++ database/migrations/2020_10_23_173424_crate_post_tag_table.php
@@ -13,7 +13,25 @@ class CratePostTagTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('post_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+
+            $table->timestamps();
+
+            //relaciones
+
+            $table->foreign('post_id')->references('id')->on('posts')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('tag_id')->references('id')->on('tags')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+             });
     }
 
     /**
